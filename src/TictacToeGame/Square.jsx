e5fe647e0b94e89b261a6c7a0c3c29f0f073d211
Priyansh kushwaha <priyanshkushwaha10@gmail.com>
2023-02-16 15:38:28 +0530

    HalfprojectEnd

---
+++ src/TictacToeGame/Square.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const Square = (props)=>{
+    return(
+        <div 
+        style={{
+        border: "1px solid",
+        height: "100px",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center", 
+        alignItems: "center"
+}} 
+        className="square">
+            {/* <h5>X</h5> */}
+        </div>
+    )
+}
+
+export default Square;
